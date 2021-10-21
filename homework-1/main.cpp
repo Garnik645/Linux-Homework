e32b68cc48ab645244a0f58adf23d4089c6c2c14
Garnik645 <garnik645@gmail.com>
2021-10-21 23:34:38 +0400

    comments were added to the main.cpp file

--- homework-1/main.cpp
+++ homework-1/main.cpp
@@ -4,6 +4,7 @@
 
 int main()
 {
+	// creating a vector for complex numbers
 	std::vector<complex> v;
 	v.push_back(complex(1, 2));
 	v.push_back(complex(21, -2));
@@ -11,7 +12,11 @@ int main()
 	v.push_back(complex(0, 1));
 	v.push_back(complex(23, 59));
 	v.push_back(complex(-4, 4));
+	
+	// sorting vector
 	sort_complex(v);
+	
+	// printing vector
 	for(complex c : v)
 	{
 		std::cout << '(' << c.real() << ", " << c.img() << ")\n";
