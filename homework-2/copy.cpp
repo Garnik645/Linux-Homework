6c5b46d12d7b743dd523963d6b1e2810ccc54220
Garnik645 <garnik645@gmail.com>
2021-11-02 22:12:18 +0400

    in copy.cpp copyfd struct was created

--- homework-2/copy.cpp
+++ homework-2/copy.cpp
@@ -6,60 +6,79 @@
 
 #define BUFFER_SIZE 16
 
-int main(int argc, char** argv)
+// struct for storing source and destination file descriptor
+struct copyfd
 {
-	// check if we have right amount of arguments to read and write to
-	if(argc < 2)
-	{
-		std::cerr << "No file to read" << std::endl;
-		exit(1);
-	}
-
-	// get input source file path
-	const char* filename1 = argv[1];
-
-	// open source file only for reading
-	int src = open(filename1, O_RDONLY);
-	
-	// check if source file was not opened due to error
-	if(src < 0)
-	{
-		std::cerr << "Something went wrong while opening the source file. Error " << errno << std::endl;
-		exit(errno);
-	}
-	
-	// variable for destination file descriptor
+	int src;
 	int dst;
 
-	// open file if destination path was given
-	if(argc > 2)
+	// open files and get file descriptor
+	copyfd(int margc, char** margv)
 	{
-		// get input destination file path
-		const char* filename2 = argv[2];
+		// check if source path argument was given
+		if(margc < 2)
+		{
+			std::cerr << "No file to read" << std::endl;
+			exit(1);
+		}
 
-		// open destination file only for writing
-		dst = open(filename2, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
+		// get input source file path
+		const char* filename1 = margv[1];
+
+		// open source file only for reading
+		src = open(filename1, O_RDONLY);
 		
-		// check if file was not opened due to error
-		if(dst < 0)
+		// check if source file was not opened due to error
+		if(src < 0)
 		{
-			std::cerr << "Something went wrong while opening the destination file. Error " << errno << std::endl;
+			std::cerr << "Something went wrong while opening the source file. Error " << errno << std::endl;
 			exit(errno);
 		}
-	}
-	// if not create destination file
-	else
-	{
-		// create destination.txt file
-		dst = creat("destination.txt", S_IRUSR | S_IWUSR);
 
-		// check if file was not created due to error
-		if(dst < 0)
+		// open file if destination path was given
+		if(margc > 2)
 		{
-			std::cerr << "Something went wrong while creating the destination file. Error " << errno << std::endl;
-			exit(errno);
+			// get input destination path
+			const char* filename2 = margv[2];
+
+			// open destination file only for writing, delete its content
+			// if it doesn't exist, create one
+			dst = open(filename2, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
+			
+			// check if file was not opened due to error
+			if(dst < 0)
+			{
+				std::cerr << "Something went wrong while opening the destination file. Error " << errno << std::endl;
+				exit(errno);
+			}
+		}
+		// if not, create destination file
+		else
+		{
+			// create destination.txt file
+			dst = creat("destination.txt", S_IRUSR | S_IWUSR);
+
+			// check if file was not created due to error
+			if(dst < 0)
+			{
+				std::cerr << "Something went wrong while creating the destination file. Error " << errno << std::endl;
+				exit(errno);
+			}
 		}
 	}
+
+	// close source and destination files
+	~copyfd()
+	{
+		close(src);
+		close(dst);
+	}
+};
+
+int main(int argc, char** argv)
+{
+	// get and store file descriptor of source and destination files
+	copyfd fd(argc, argv);
 
 	// allocate a buffer to read from file
 	char* buffer = new char[BUFFER_SIZE + 1];
@@ -68,7 +87,7 @@ int main(int argc, char** argv)
 	while(true)
 	{
 		// read BUFFER_SIZE bytes from source file into buffer
-		int readBytes = read(src, buffer, BUFFER_SIZE);
+		int readBytes = read(fd.src, buffer, BUFFER_SIZE);
 		
 		// check if could not read due to error
 		if(readBytes < 0)
@@ -82,7 +101,7 @@ int main(int argc, char** argv)
 			break;
 		
 		// write content of buffer into destination file
-		int written = write(dst, buffer, readBytes);
+		int written = write(fd.dst, buffer, readBytes);
 		
 		// check if could not write due to error
 		if(written < 0)
@@ -94,10 +113,6 @@ int main(int argc, char** argv)
 
 	// delete allocated memory	
 	delete[] buffer;
-
-	// close source and destination files		
-	close(src);
-	close(dst);
 
 	return 0;
 }
