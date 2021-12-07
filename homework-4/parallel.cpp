82ec6787e1ad8184b530e605ad76ad94ceda2705
Garnik Khroyan <garnik645@gmail.com>
2021-12-07 15:04:50 +0000

    comments were added

--- homework-4/parallel.cpp
+++ homework-4/parallel.cpp
@@ -2,26 +2,34 @@
 
 int main()
 {
+    // create 2000 x 4000 matrix
     parallel_matrix mtrx(2000, 4000);
+    
+    // initialize matrix with random numbers
     mtrx.init();
     
+    // calculate mtrx.sum() and measure time
     std::cout << "--- mtrx.sum() ---" << std::endl;
     measure_time(std::bind(&parallel_matrix::sum, mtrx));
     std::cout << std::endl;
     
+    // bind and generate a function class to pass to other function to measure time of their work
     auto ft = std::bind(&parallel_matrix::sum_parallel, mtrx, std::placeholders::_1);
 
+    // calculate mtrx.sum_parallel(8) and measure time
     std::cout << "--- mtrx.sum_parallel(8) ---" << std::endl;
     measure_time(ft, 8);
     std::cout << std::endl;
 
+    // calculate mtrx.sum_parallel(16) and measure time
     std::cout << "--- mtrx.sum_parallel(16) ---" << std::endl;
     measure_time(ft, 16);
     std::cout << std::endl;
 
+    // calculate mtrx.sum_parallel(100) and measure time
     std::cout << "--- mtrx.sum_parallel(100) ---" << std::endl;
     measure_time(ft, 100);
     std::cout << std::endl;
-    
+
     return 0;
 }
--- homework-4/parallel_matrix_lib/parallel_matrix.cpp
+++ homework-4/parallel_matrix_lib/parallel_matrix.cpp
@@ -1,10 +1,19 @@
 #include "parallel_matrix.h"
 
+// arg struct to pass to threads
 struct arg
 {
+    // pointer to matrix
     std::vector<std::vector<int>>* data = nullptr;
+
+    // thread calculates matrix elements with index
+    // rest, t + rest, 2 * t + rest, ...
     size_t rest = 0;
+
+    // number of threads
     size_t t = 0;
+
+    // basic constructor
     arg(std::vector<std::vector<int>>* _data = nullptr, size_t _rest = 0, size_t _t = 0)
         : data(_data)
         , rest(_rest)
@@ -12,14 +21,28 @@ struct arg
         {}
 };
 
+// calculate local sum for each thread
 void* thread_sum(void* in)
 {
+    // store result
     int ans = 0;
+
+    // get input
     arg* th_arg = (arg*) in;
     std::vector<std::vector<int>>* th_mtrx = th_arg->data;
+    
+    // number of columns
     size_t col = th_mtrx->size();
+
+    // number of rows
     size_t row = (th_mtrx->at(0)).size();
+
+    // number of elements in the matrix
     size_t el_count = col * row;
+    
+    // go through elements with index
+    // rest, t + rest, 2 * t + rest, ...
+    // and add to the result
     for(size_t l = th_arg->rest; l < el_count; l += th_arg->t)
     {
         size_t i = l / row;
@@ -29,6 +52,8 @@ void* thread_sum(void* in)
     return (void*) new int(ans);
 }
 
+// matrix constructor
+// create a matrix with size N x M
 parallel_matrix::parallel_matrix(size_t n, size_t m)
 {
     assert(n > 0 && m > 0);
@@ -39,6 +64,7 @@ parallel_matrix::parallel_matrix(size_t n, size_t m)
     }
 }
 
+// initialize matrix with random numbers
 void parallel_matrix::init()
 {
     std::random_device dev;
@@ -53,6 +79,7 @@ void parallel_matrix::init()
     }
 }
 
+// calculate sum of all the elements in the matrix without additional threads
 int parallel_matrix::sum()
 {
     int result = 0;
@@ -66,40 +93,72 @@ int parallel_matrix::sum()
     return result;
 }
 
+// calculate sum of all the elements in the matrix t threads
 int parallel_matrix::sum_parallel(size_t t)
 {
+    // check if t is a natural number
     assert(t > 0);
+
+    // store the result
     int result = 0;
+
+    // allocate memory for thread ID
     pthread_t* threads = new pthread_t[t];
+
+    // allocate memory for arguments
     arg* thread_args = new arg[t];
+
+    // divide matrix into pieces and create threads
     for(size_t i = 0; i < t; ++i)
     {
+        // store argument
         thread_args[i] = arg(&mtrx, i, t);
+
+        // create a thread
         int thread_out = pthread_create(&threads[i], NULL, thread_sum, &thread_args[i]);
+
+        // check for errors
         if(thread_out != 0)
         {
             std::cerr << "Error while creating thread!" << std::endl;
             exit(thread_out);
         }
     }
+
+    // memory to store the result of the local sum of the thread
     void* thread_result;
+
+    // for every thread
     for(size_t i = 0; i < t; ++i)
     {
+        // join thread
         int thread_out = pthread_join(threads[i], &thread_result);
+
+        // check for errors
         if(thread_out != 0)
         {
             std::cerr << "Error while joining thread!" << std::endl;
             exit(thread_out);
         }
+
+        // convert void* into int*
         int* thread_result_int = (int*) thread_result;
+
+        // add local sum to the result
         result += *thread_result_int;
+
+        // free allocated memory
         delete thread_result_int;
     }
+
+    // free allocated memory
     delete[] threads;
     delete[] thread_args;
+
     return result;
 }
 
+// function to measure the work time of other functions
 void measure_time(std::function<int(int)> func, int x)
 {
     auto start = std::chrono::high_resolution_clock::now();
@@ -109,6 +168,7 @@ void measure_time(std::function<int(int)> func, int x)
     std::cout << "\tmeasured time in microseconds : " << duration.count() << std::endl;
 }
 
+// function to measure the work time of other functions
 void measure_time(std::function<int()> func)
 {
     auto start = std::chrono::high_resolution_clock::now();
