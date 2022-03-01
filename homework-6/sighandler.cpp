f9dead2647180cd2adc9292b1344eb41ed35a568
Garnik Khroyan <garnik.khroyan@ispras.ru>
2022-03-01 23:06:32 +0400

    homework-6 was added

---
+++ homework-6/sighandler.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <csignal>
+#include <cstdlib>
+#include <ucontext.h>
+#include <string>
+#include <cerrno>
+#include <cstring>
+#include <pwd.h>
+
+void handler(int signo, siginfo_t *info, void *vcontext)
+{
+    std::cout << "Signal Handler for signal N-" << signo << std::endl;
+    auto* context = static_cast<ucontext_t*>(vcontext);
+    std::cout << "-- PID of the sender : " << info->si_pid << std::endl;
+    std::cout << "-- UID of the sender : " << info->si_uid << std::endl;
+    struct passwd *pw = getpwuid (info->si_uid);
+    auto name = static_cast<std::string>(pw->pw_name);
+    std::cout << "-- User name : " << name << std::endl;
+    auto mcontext = context->uc_mcontext;
+    std::cout << "-- RIP register : " << mcontext.gregs[REG_RIP] << std::endl;
+    std::cout << "-- RAX register : " << mcontext.gregs[REG_RAX] << std::endl;
+    std::cout << "-- RBX register : " << mcontext.gregs[REG_RBX] << std::endl;
+}
+
+int main()
+{
+    struct sigaction act{};
+    memset(&act, 0, sizeof(act));
+
+    act.sa_flags = SA_SIGINFO;
+    act.sa_sigaction = &handler;
+    if (sigaction(SIGUSR1, &act, nullptr) == -1) {
+        std::cerr << "sigaction fail with error " << errno << std::endl;
+        exit(errno);
+    }
+
+    raise(SIGUSR1);
+}
